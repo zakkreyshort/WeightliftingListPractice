5d11be99385ef16b006d8182a4bab12e99eb2770
Zakkrey Short <zakkreyjshort@gmail.com>
2020-05-28 10:26:43 -0700

    successfully create weight database connection to mySQL

---
+++ WeightliftingList/Migrations/20200528172608_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeightliftingList.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Weights",
+                columns: table => new
+                {
+                    WeightId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    WeightType = table.Column<string>(nullable: true),
+                    Pounds = table.Column<int>(nullable: false),
+                    Material = table.Column<string>(nullable: true),
+                    Brand = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weights", x => x.WeightId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weights");
+        }
+    }
+}
---
+++ WeightliftingList/Models/Weight.cs
@@ -0,0 +1,11 @@
+namespace WeightliftingList.Models
+{
+    public class Weight
+    {
+        public int WeightId { get; set; }
+        public string WeightType { get; set; }
+        public int Pounds { get; set; }
+        public string Material { get; set; }
+        public string Brand { get; set; }
+    }
+}
