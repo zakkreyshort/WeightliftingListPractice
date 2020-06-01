00dc9126f02f97d68d95c395385a3dc1435d18b0
Zakkrey Short <zakkreyjshort@gmail.com>
2020-06-01 09:24:19 -0700

    add validators to weight

--- WeightliftingList/Models/Weight.cs
+++ WeightliftingList/Models/Weight.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WeightliftingList.Models
 {
     public class Weight
     {
         public int WeightId { get; set; }
+        [Required]
+        [StringLength(45)]
         public string WeightType { get; set; }
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Weight must be valid and in pounds")]
         public int Pounds { get; set; }
+        [Required]
         public string Material { get; set; }
+        [Required]
         public string Brand { get; set; }
     }
 }
